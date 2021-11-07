dc9b0c7d603c937543f6ac76dd2d98e66ab03a36
Vicente <vicentedpsantos@gmail.com>
2021-11-07 01:21:34 +0000

    Enable the A20 line (https://wiki.osdev.org/A20_Line)

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -33,7 +33,7 @@ step2:
   mov cr0, eax
   jmp CODE_SEG:load32
 
-; GDT (Global Descriptor Table)
+; GDT (Global Descriptor Table https://wiki.osdev.org/Global_Descriptor_Table)
 gdt_start:
 gdt_null:
   dd 0x0
@@ -72,6 +72,12 @@ load32:
   mov ss, ax
   mov ebp, 0x00200000
   mov esp, ebp
+
+  ; Enable the A20 line (https://wiki.osdev.org/A20_Line)
+  in al, 0x92
+  or al, 2
+  out 0x92, al
+
   jmp $
 
 times 510-($ - $$) db 0
