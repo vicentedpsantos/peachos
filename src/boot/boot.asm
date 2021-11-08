8c64b0d0bfb3926952c99aa73b5faae006e8477d
Vicente <vicentedpsantos@gmail.com>
2021-11-08 00:20:35 +0000

    Loading 32 bit kernel into memory and working with debugging symbols

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -32,6 +32,7 @@ step2:
   or eax, 0x1
   mov cr0, eax
   jmp CODE_SEG:load32
+  jmp $
 
 ; GDT (Global Descriptor Table https://wiki.osdev.org/Global_Descriptor_Table)
 gdt_start:
@@ -64,21 +65,70 @@ gdt_descriptor:
 
 [BITS 32]
 load32:
-  mov ax, DATA_SEG
-  mov ds, ax
-  mov es, ax
-  mov fs, ax
-  mov gs, ax
-  mov ss, ax
-  mov ebp, 0x00200000
-  mov esp, ebp
+  mov eax, 1
+  mov ecx, 100
+  mov edi, 0x0100000
+  call ata_lba_read
+  jmp CODE_SEG:0x0100000
 
-  ; Enable the A20 line (https://wiki.osdev.org/A20_Line)
-  in al, 0x92
-  or al, 2
-  out 0x92, al
+ata_lba_read:
+  mov ebx, eax, ; Backup the LBA
+  ; Send the highest 8 bits of the lba to the hard disk controller
+  shr eax, 24
+  or eax, 0xE0 ; Selects the master drive
+  mov dx, 0x1F6
+  out dx, al
+  ; Finished sending the highest 8 bits of the lba
 
-  jmp $
+  ; Send the total sectors to read
+  mov eax, ecx
+  mov dx, 0x1F2
+  out dx, al
+  ; Finished sending the total sectors to read
+
+  ; Send more bits of the lba
+  mov eax, ebx ; Restore the backup lba
+  mov dx, 0x1F3
+  out dx, al
+  ; Finished sending more bits of the lba
+
+  ; Send more bits of the lba
+  mov dx, 0x1F4
+  mov eax, ebx ; Restore the backup lba
+  shr eax, 8
+  out dx, al
+  ; Finished sending more bits of the lba
+
+  ; Send upper 16 bits of the lba
+  mov dx, 0x1F5
+  mov eax, ebx ; Restore the back up lba
+  shr eax, 16
+  out dx, al
+  ; Finished sending 16 bits of the lba
+
+  mov dx, 0x1f7
+  mov al, 0x20
+  out dx, al
+
+  ; Read all sectors into memory
+.next_sector:
+  push ecx
+
+; Checking if we need to read
+.try_again:
+  mov dx, 0x1f7
+  in al, dx
+  test al, 8
+  jz .try_again
+
+; We need to read 256 words at a time
+  mov ecx, 256
+  mov dx, 0x1F0
+  rep insw
+  pop ecx
+  loop .next_sector
+  ; End of reading sectors into memory
+  ret
 
 times 510-($ - $$) db 0
 dw 0xAA55
