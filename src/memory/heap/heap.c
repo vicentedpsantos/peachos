546816f9ed6f8dd7639aa6fd3784123a794b85e1
Vicente <vicentedpsantos@gmail.com>
2021-11-14 16:07:59 +0000

    Format with ccls

--- src/memory/heap/heap.c
+++ src/memory/heap/heap.c
@@ -1,17 +1,15 @@
 #include "heap.h"
 #include "kernel.h"
-#include "status.h"
 #include "memory/memory.h"
+#include "status.h"
 #include <stdbool.h>
 
-static int heap_validate_table(void* ptr, void* end, struct heap_table* table)
-{
+static int heap_validate_table(void *ptr, void *end, struct heap_table *table) {
   int res = 0;
   size_t table_size = (size_t)(end - ptr);
   size_t total_blocks = table_size / PEACH_OS_HEAP_BLOCK_SIZE;
 
-  if (table->total != total_blocks)
-  {
+  if (table->total != total_blocks) {
     res = -EINVARG;
     goto out;
   }
@@ -19,17 +17,15 @@ out:
   return res;
 }
 
-static bool heap_validate_alignment(void* ptr)
-{
+static bool heap_validate_alignment(void *ptr) {
   return ((unsigned int)ptr % PEACH_OS_HEAP_BLOCK_SIZE) == 0;
 }
 
-int heap_create(struct heap* heap, void* ptr, void* end, struct heap_table* table)
-{
+int heap_create(struct heap *heap, void *ptr, void *end,
+                struct heap_table *table) {
   int res = 0;
 
-  if (!heap_validate_alignment(ptr) || !heap_validate_alignment(end))
-  {
+  if (!heap_validate_alignment(ptr) || !heap_validate_alignment(end)) {
     res = -EINVARG;
     goto out;
   }
@@ -39,8 +35,7 @@ int heap_create(struct heap* heap, void* ptr, void* end, struct heap_table* tabl
   heap->table = table;
 
   res = heap_validate_table(ptr, end, table);
-  if (res < 0)
-  {
+  if (res < 0) {
     goto out;
   }
 
@@ -50,10 +45,8 @@ out:
   return res;
 }
 
-static uint32_t heap_align_value_to_upper(uint32_t val)
-{
-  if (val % PEACH_OS_HEAP_BLOCK_SIZE == 0)
-  {
+static uint32_t heap_align_value_to_upper(uint32_t val) {
+  if (val % PEACH_OS_HEAP_BLOCK_SIZE == 0) {
     return val;
   }
 
@@ -62,78 +55,66 @@ static uint32_t heap_align_value_to_upper(uint32_t val)
   return val;
 }
 
-static int heap_get_entry_type(HEAP_BLOCK_TABLE_ENTRY entry)
-{
+static int heap_get_entry_type(HEAP_BLOCK_TABLE_ENTRY entry) {
   return entry & 0x0f;
 }
 
-int heap_get_start_block(struct heap* heap, uint32_t total_blocks)
-{
-  struct heap_table* table = heap->table;
+int heap_get_start_block(struct heap *heap, uint32_t total_blocks) {
+  struct heap_table *table = heap->table;
   int bc = 0;
   int bs = -1;
 
-  for(size_t i = 0; i < table->total; i++)
-  {
-    if (heap_get_entry_type(table->entries[i]) != HEAP_BLOCK_TABLE_ENTRY_FREE)
-    {
+  for (size_t i = 0; i < table->total; i++) {
+    if (heap_get_entry_type(table->entries[i]) != HEAP_BLOCK_TABLE_ENTRY_FREE) {
       bc = 0;
       bs = -1;
       continue;
     }
 
     // If this is the first block
-    if (bs == -1)
-    {
+    if (bs == -1) {
       bs = i;
     }
 
     bc++;
-    if (bc == total_blocks)
-    {
+    if (bc == total_blocks) {
       break;
     }
   }
 
-  if (bs == -1)
-  {
+  if (bs == -1) {
     return -ENOMEM;
   }
 
   return bs;
 }
 
-void* heap_block_to_address(struct heap* heap, int block)
-{
+void *heap_block_to_address(struct heap *heap, int block) {
   return heap->saddr + (block * PEACH_OS_HEAP_BLOCK_SIZE);
 }
 
-void heap_mark_blocks_taken(struct heap* heap, int start_block, int total_blocks)
-{
+void heap_mark_blocks_taken(struct heap *heap, int start_block,
+                            int total_blocks) {
   int end_block = start_block + total_blocks - 1;
-  HEAP_BLOCK_TABLE_ENTRY entry = HEAP_BLOCK_TABLE_ENTRY_TAKEN | HEAP_BLOCK_IS_FIRST;
-  if (total_blocks > 1)
-  {
+  HEAP_BLOCK_TABLE_ENTRY entry =
+      HEAP_BLOCK_TABLE_ENTRY_TAKEN | HEAP_BLOCK_IS_FIRST;
+  if (total_blocks > 1) {
     entry |= HEAP_BLOCK_HAS_NEXT;
   }
 
-  for (int i = start_block; i <= end_block; i++)
-  {
+  for (int i = start_block; i <= end_block; i++) {
     heap->table->entries[i] = entry;
     entry = HEAP_BLOCK_TABLE_ENTRY_TAKEN;
-    if (i != end_block -1)
-    {
+    if (i != end_block - 1) {
       entry |= HEAP_BLOCK_HAS_NEXT;
     }
   }
 }
 
-void* heap_malloc_blocks(struct heap* heap, uint32_t total_blocks)
-{
-  void* address = 0;
+void *heap_malloc_blocks(struct heap *heap, uint32_t total_blocks) {
+  void *address = 0;
   int start_block = heap_get_start_block(heap, total_blocks);
-  if (start_block < 0)
-  {
+  if (start_block < 0) {
     goto out;
   }
 
@@ -145,33 +126,27 @@ out:
   return address;
 }
 
-void heap_mark_blocks_free(struct heap* heap, int starting_block)
-{
-  struct heap_table* table = heap->table;
-  for (int i = starting_block; i < (int)table->total; i++)
-  {
+void heap_mark_blocks_free(struct heap *heap, int starting_block) {
+  struct heap_table *table = heap->table;
+  for (int i = starting_block; i < (int)table->total; i++) {
     HEAP_BLOCK_TABLE_ENTRY entry = table->entries[i];
     table->entries[i] = HEAP_BLOCK_TABLE_ENTRY_FREE;
-    if (!(entry & HEAP_BLOCK_HAS_NEXT))
-    {
+    if (!(entry & HEAP_BLOCK_HAS_NEXT)) {
       break;
     }
   }
 }
 
-int heap_address_to_block(struct heap* heap, void* address)
-{
+int heap_address_to_block(struct heap *heap, void *address) {
   return ((int)(address - heap->saddr)) / PEACH_OS_HEAP_BLOCK_SIZE;
 }
 
-void* heap_malloc(struct heap* heap, size_t size)
-{
+void *heap_malloc(struct heap *heap, size_t size) {
   size_t aligned_size = heap_align_value_to_upper(size);
   uint32_t total_blocks = aligned_size / PEACH_OS_HEAP_BLOCK_SIZE;
   return heap_malloc_blocks(heap, total_blocks);
 }
 
-void heap_free(struct heap* heap, void* ptr)
-{
+void heap_free(struct heap *heap, void *ptr) {
   heap_mark_blocks_free(heap, heap_address_to_block(heap, ptr));
 }
--- src/memory/heap/kheap.c
+++ src/memory/heap/kheap.c
@@ -1,31 +1,25 @@
 #include "kheap.h"
-#include "heap.h"
 #include "config.h"
+#include "heap.h"
 #include "kernel.h"
 
 struct heap kernel_heap;
 struct heap_table kernel_heap_table;
 
-void kheap_init()
-{
+void kheap_init() {
   int total_table_entries = PEACH_OS_HEAP_SIZE_BYTES / PEACH_OS_HEAP_BLOCK_SIZE;
-  kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY*)(PEACH_OS_HEAP_TABLE_ADDRESS);
+  kernel_heap_table.entries =
+      (HEAP_BLOCK_TABLE_ENTRY *)(PEACH_OS_HEAP_TABLE_ADDRESS);
   kernel_heap_table.total = total_table_entries;
 
-  void* end = (void*)(PEACH_OS_HEAP_ADDRESS + PEACH_OS_HEAP_SIZE_BYTES);
-  int res = heap_create(&kernel_heap, (void*)(PEACH_OS_HEAP_ADDRESS), end, &kernel_heap_table);
-  if (res < 0)
-  {
+  void *end = (void *)(PEACH_OS_HEAP_ADDRESS + PEACH_OS_HEAP_SIZE_BYTES);
+  int res = heap_create(&kernel_heap, (void *)(PEACH_OS_HEAP_ADDRESS), end,
+                        &kernel_heap_table);
+  if (res < 0) {
     print("Failed to create heap\n");
   }
 }
 
-void* kmalloc(size_t size)
-{
-  return heap_malloc(&kernel_heap, size);
-}
+void *kmalloc(size_t size) { return heap_malloc(&kernel_heap, size); }
 
-void kfree(void* ptr)
-{
-  heap_free(&kernel_heap, ptr);
-}
+void kfree(void *ptr) { heap_free(&kernel_heap, ptr); }
