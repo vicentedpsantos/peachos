eca3858bcafc337fa105cdf5f70b5535610800cf
Vicente <vicentedpsantos@gmail.com>
2021-11-14 15:54:37 +0000

    Remove heap test pointers

--- src/kernel.c
+++ src/kernel.c
@@ -83,17 +83,4 @@ void kernel_main()
 
   // Initialize the interrupt descriptor table
   idt_init();
-
-  void* ptr = kmalloc(50);
-  void* ptr2 = kmalloc(5000);
-  void* ptr3 = kmalloc(5600);
-
-  kfree(ptr);
-
-  void* ptr4 = kmalloc(50);
-
-  if(ptr || ptr2 || ptr3 || ptr4)
-  {
-    
-  }
 }
