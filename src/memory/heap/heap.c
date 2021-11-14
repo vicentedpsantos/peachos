005f2206d515b6b705d1a48b234d5e06346447e4
Vicente <vicentedpsantos@gmail.com>
2021-11-14 15:14:11 +0000

    Create function to locate the start block in the heap

--- src/memory/heap/heap.c
+++ src/memory/heap/heap.c
@@ -50,12 +50,84 @@ out:
   return res;
 }
 
-void* heap_malloc(size_t size)
+static uint32_t heap_align_value_to_upper(uint32_t val)
 {
-  return 0;
+  if (val % PEACH_OS_HEAP_BLOCK_SIZE == 0)
+  {
+    return val;
+  }
+
+  val = (val - (val & PEACH_OS_HEAP_BLOCK_SIZE));
+  val += PEACH_OS_HEAP_BLOCK_SIZE;
+  return val;
 }
 
-void heap_free(void* ptr)
+static int heap_get_entry_type(HEAP_BLOCK_TABLE_ENTRY entry)
+{
+  return entry & 0x0f;
+}
+
+int heap_get_start_block(struct heap* heap, uint32_t total_blocks)
+{
+  struct heap_table* table = heap->table;
+  int bc = 0;
+  int bs = -1;
+
+  for(size_t i = 0; i < table->total; i++)
+  {
+    if (heap_get_entry_type(table->entries[i]) != HEAP_BLOCK_TABLE_ENTRY_FREE)
+    {
+      bc = 0;
+      bs = -1;
+      continue;
+    }
+
+    // If this is the first block
+    if (bs == -1)
+    {
+      bs = i;
+    }
+
+    bc++;
+    if (bc == total_blocks)
+    {
+      break;
+    }
+  }
+
+  if (bs == -1)
+  {
+    return -ENOMEM;
+  }
+
+  return bs;
+}
+
+void* heap_malloc_blocks(struct heap* heap, uint32_t total_blocks)
+{
+  void* address = 0;
+  int start_block = heap_get_start_block(heap, total_blocks);
+  if (start_block < 0)
+  {
+    goto out;
+  }
+
+  address = heap_block_to_address(heap, start_block);
+
+  // Mark the blocks as taken
+  heap_mark_blocks_taken(heap, start_block, total_blocks);
+out:
+  return address;
+}
+
+void* heap_malloc(struct heap* heap, size_t size)
+{
+  size_t aligned_size = heap_align_value_to_upper(size);
+  uint32_t total_blocks = aligned_size / PEACH_OS_HEAP_BLOCK_SIZE;
+  return heap_malloc_blocks(heap, total_blocks);
+}
+
+void heap_free(struct heap* heap, void* ptr)
 {
   return 0;
 }
--- src/status.h
+++ src/status.h
@@ -4,4 +4,5 @@
 #define PEACH_OS_ALL_OK 0
 #define EIO 1
 #define EINVARG 2
+#define ENOMEM 3
 #endif
