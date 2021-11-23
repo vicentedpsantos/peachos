aec729db826c410faa469d981653e2669fe4f4c4
Vicente <vicentedpsantos@gmail.com>
2021-11-23 18:58:42 +0000

    Pagination get indices

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -2,6 +2,7 @@
 
 #include "paging.h"
 #include "memory/heap/kheap.h"
+#include "status.h"
 
 void paging_load_directory(uint32_t *directory);
 static uint32_t *current_directory = 0;
@@ -36,4 +37,23 @@ void paging_switch(uint32_t *directory) {
 
 uint32_t *paging_4gb_chunk_get_directory(struct paging_4gb_chunk *chunk) {
   return chunk->directory_entry;
+}
+
+bool paging_is_aligned(void *address) {
+  return ((uint32_t)address % PAGING_PAGE_SIZE) == 0
+}
+
+int paging_get_indexes(void *virtual_address, uint32_t *directory_index_out,
+                       uint32_t *table_index_out) {
+  int res = 0;
+
+  if (!paging_is_aligned(virtual_address)) {
+    res = -EINVARG;
+    goto out;
+  }
+
+  *directory_index_out = ((uint32_t)virtual_address / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
+  *table_index_out = ((uint32_t)virtual_address % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
+out:
+  return res;
 }
